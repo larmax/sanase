86e63b8d70c09e455964593faf3ece1f625bc73e
larmax <leon.miller@hotmail.fi>
2019-01-03 11:09:13 +0200

    reading done hopefully

--- getPagesSource.js
+++ getPagesSource.js
@@ -31,18 +31,30 @@ function findNames(html) {
 var namesArr = [];
 var titlesAndCompaniesArr = [];
 var locationsArr = [];
+var titlesArr = [];
   var names = document.getElementsByTagName('h3');
 var titlesAndCompanies = document.getElementsByClassName('Sans-14px-black-75%-bold')
 var locations = document.getElementsByClassName('result-lockup__misc-item')
-
+var titles = document.getElementsByTagName('span')
+console.log('titles',titles);
   console.log('names',names,'titlesAndCompanies', titlesAndCompanies,'locations',locations);
   for(var i = 0; i < names.length; i++) {
 
-          locationsArr.push(locations[i].innerHTML)
-          namesArr.push(names[i].innerHTML)
-  
+if (i > 5 && i < 31) {
+
+      namesArr.push(names[i].innerHTML)
+}
+
+  }
+  for (var i = 0; i < locations .length; i++) {
+
+  locationsArr.push(locations[i].innerHTML)
   }
   console.log('locationsArr',locationsArr);
+  // for (var i = 0; i < titles.length; i++) {
+  // titlesArr.push(titles[i].innerHTML)
+  // }
+  // console.log('titlesArr',titlesArr);
   for(var i = 0; i < titlesAndCompanies.length; i++) {
 
       titlesAndCompaniesArr.push(titlesAndCompanies[i].innerHTML)
@@ -50,14 +62,17 @@ var locations = document.getElementsByClassName('result-lockup__misc-item')
   }
 
 console.log('titlesAndCompaniesArr',titlesAndCompaniesArr,"length",titlesAndCompaniesArr.length,'' );
-titlesArr = [];
+
 companiesArr = [];
 for (var i = 0; i < titlesAndCompaniesArr.length; i++) {
-  if (titlesAndCompaniesArr[i].includes("<b>")) {
+
+  if (!titlesAndCompaniesArr[i].includes("<") && i > 2) {
+    console.log('title');
 titlesArr.push(titlesAndCompaniesArr[i])
 
   }
   if (titlesAndCompaniesArr[i].includes("Go to")) {
+    console.log('company');
 companiesArr.push(titlesAndCompaniesArr[i])
 
   }
@@ -66,14 +81,27 @@ companiesArr.push(titlesAndCompaniesArr[i])
 
 console.log('titlesArr berfor',titlesArr,'companiesArr before',companiesArr);
 var regex = /(<([^>]+)>)/ig;
-for(var i=0;i<titlesArr.length;i++){
+for(var i=0; i < namesArr.length; i++){
+  console.log('hwat');
   namesArr[i] = namesArr[i].replace('Profile result - ', 'name: ')
-     titlesArr[i] = titlesArr[i].replace(regex, "").replace('amp;','');
-      companiesArr[i] = companiesArr[i].replace(regex, "").replace(/(\r\n|\n|\r|)/gm, "").replace(/\Go to.*/,'');
+
 
   }
+  console.log('namesArr',namesArr);
+  for(var i=0; i < titlesArr.length; i++){
+    console.log('hwat');
 
-console.log('titlesArr after',titlesArr,'companiesArr before',companiesArr);
+       titlesArr[i] = titlesArr[i].replace(regex, "").replace('amp;','');
+
+    }
+    for(var i=0; i < companiesArr.length; i++){
+      console.log('hwat');
+
+          companiesArr[i] = companiesArr[i].replace(regex, "").replace(/(\r\n|\n|\r|)/gm, "").replace(/\Go to.*/,'').replace('                  ','').replace('                          ','');
+
+      }
+
+console.log('titlesArr after',titlesArr,'companiesArr after',companiesArr);
   const l = Math.min(namesArr.length, titlesArr.length, companiesArr.length, locationsArr.length);
   const merged = [].concat(...Array.from({ length: l }, (_, i) => [namesArr[i], titlesArr[i],companiesArr[i],locationsArr[i]]), namesArr.slice(l), titlesArr.slice(l), companiesArr.slice(l),locationsArr.slice(l));
 return merged;
