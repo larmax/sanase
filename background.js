1f7b06c71ed9c7ee6babc45bae09c1e251cbfeb5
larmax <leon.miller@hotmail.fi>
2019-01-03 15:17:27 +0200

    scroll test

--- background.js
+++
@@ -1,5 +0,0 @@
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var action_url = "javascript:window.print();";
-  chrome.tabs.update(tab.id, {url: action_url});
-});
--- writeToDocument.js
+++
@@ -1,9 +0,0 @@
-chrome.windows.create({
-    type: 'popup',
-    url: "https://www.google.co.in/"
-}, function (newWindow) {
-    console.log(newWindow);
-    chrome.tabs.executeScript(newWindow.tabs[0].id, {
-        code: 'document.write(merged);'
-    });
-});
