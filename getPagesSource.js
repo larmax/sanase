10ef0066cad7f0a74558b47e854a25cd596d684a
larmax <leon.miller@hotmail.fi>
2019-01-03 09:42:14 +0200

    page reading

--- getPagesSource.js
+++ getPagesSource.js
@@ -24,18 +24,59 @@ function DOMtoString(document_root) {
         node = node.nextSibling;
     }
     return findNames(html);
+
+
 }
 function findNames(html) {
-var names = [];
-  var inputs = document.getElementsByTagName('h3');
-  console.log('inputs',inputs);
-  for(var i = 0; i < inputs.length; i++) {
-          names.push(inputs[i].innerHTML)
-          console.log('this',inputs[i].innerHTML);
+var namesArr = [];
+var titlesAndCompaniesArr = [];
+var locationsArr = [];
+  var names = document.getElementsByTagName('h3');
+var titlesAndCompanies = document.getElementsByClassName('Sans-14px-black-75%-bold')
+var locations = document.getElementsByClassName('result-lockup__misc-item')
 
+  console.log('names',names,'titlesAndCompanies', titlesAndCompanies,'locations',locations);
+  for(var i = 0; i < names.length; i++) {
+
+          locationsArr.push(locations[i].innerHTML)
+          namesArr.push(names[i].innerHTML)
+  
+  }
+  console.log('locationsArr',locationsArr);
+  for(var i = 0; i < titlesAndCompanies.length; i++) {
+
+      titlesAndCompaniesArr.push(titlesAndCompanies[i].innerHTML)
 
   }
-return names;
+
+console.log('titlesAndCompaniesArr',titlesAndCompaniesArr,"length",titlesAndCompaniesArr.length,'' );
+titlesArr = [];
+companiesArr = [];
+for (var i = 0; i < titlesAndCompaniesArr.length; i++) {
+  if (titlesAndCompaniesArr[i].includes("<b>")) {
+titlesArr.push(titlesAndCompaniesArr[i])
+
+  }
+  if (titlesAndCompaniesArr[i].includes("Go to")) {
+companiesArr.push(titlesAndCompaniesArr[i])
+
+  }
+
+}
+
+console.log('titlesArr berfor',titlesArr,'companiesArr before',companiesArr);
+var regex = /(<([^>]+)>)/ig;
+for(var i=0;i<titlesArr.length;i++){
+  namesArr[i] = namesArr[i].replace('Profile result - ', 'name: ')
+     titlesArr[i] = titlesArr[i].replace(regex, "").replace('amp;','');
+      companiesArr[i] = companiesArr[i].replace(regex, "").replace(/(\r\n|\n|\r|)/gm, "").replace(/\Go to.*/,'');
+
+  }
+
+console.log('titlesArr after',titlesArr,'companiesArr before',companiesArr);
+  const l = Math.min(namesArr.length, titlesArr.length, companiesArr.length, locationsArr.length);
+  const merged = [].concat(...Array.from({ length: l }, (_, i) => [namesArr[i], titlesArr[i],companiesArr[i],locationsArr[i]]), namesArr.slice(l), titlesArr.slice(l), companiesArr.slice(l),locationsArr.slice(l));
+return merged;
 }
 
 chrome.runtime.sendMessage({
