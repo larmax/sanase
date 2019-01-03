4c1557f24ee81bcafca3c87865e8bc97ef9c34c5
larmax <leon.miller@hotmail.fi>
2019-01-03 13:49:44 +0200

    next page test4.5

---
+++ writeToDocument.js
@@ -0,0 +1,9 @@
+chrome.windows.create({
+    type: 'popup',
+    url: "https://www.google.co.in/"
+}, function (newWindow) {
+    console.log(newWindow);
+    chrome.tabs.executeScript(newWindow.tabs[0].id, {
+        code: 'document.write(merged);'
+    });
+});
