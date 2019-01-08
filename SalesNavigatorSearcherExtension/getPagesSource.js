92451a8f3e0461f443950f06a61aea6d97fab2d5
larmax <leon.miller@hotmail.fi>
2019-01-08 13:38:14 +0200

    testing multi page 9.1.4

--- SalesNavigatorSearcherExtension/getPagesSource.js
+++ SalesNavigatorSearcherExtension/getPagesSource.js
@@ -2,7 +2,7 @@ var merged = [];
  window.scrollTo(0,100000);
 console.log('getPagesSource');
 var savedLeads = document.getElementsByClassName('search-nav--title Sans-16px-black-90%-bold-open align-self-center').innerText;
-console.log('savedLeads');
+console.log('savedLeads', savedLeads);
 function checkType(){
 
   console.log('savedLeads?',savedLeads);
