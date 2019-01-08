e3fbea510463013c47c08dcecbc5158d484e465c
larmax <leon.miller@hotmail.fi>
2019-01-08 16:33:40 +0200

    savedleads test6.2

--- SalesNavigatorSearcherExtension/getPagesSource.js
+++ SalesNavigatorSearcherExtension/getPagesSource.js
@@ -119,7 +119,7 @@ index = JSON.stringify(i);
 
 
     titlesArr[i] = titlesArr[i].replace(regex, "").replace('amp;','').replace(/(\r\n|\n|\r)/gm,'(:(:(:');
-    console.log(titlesArr);
+    console.log(i,titlesArr);
     if (titlesArr[i].includes('(:(:(:')) {
       console.log('includes saved');
       titlesArr.splice(i, 1);
