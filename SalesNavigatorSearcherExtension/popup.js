54789f65ec3b9215e8e1840bbe3eb2277582ba57
larmax <leon.miller@hotmail.fi>
2019-01-08 10:00:39 +0200

    testing multi page7

--- SalesNavigatorSearcherExtension/popup.js
+++ SalesNavigatorSearcherExtension/popup.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
     port.postMessage({startstop:'stop'});
     console.log('stop!');
     stop = true;
-chrome.runtime.sendMessage('save');
   });
 
 startButton.addEventListener("click", function(){
