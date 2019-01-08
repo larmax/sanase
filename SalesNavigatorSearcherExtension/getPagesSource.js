0222883ea609fa6bde0a789bcddfe771a64f9f9f
larmax <leon.miller@hotmail.fi>
2019-01-08 13:31:44 +0200

    testing multi page 9.1.2

--- SalesNavigatorSearcherExtension/getPagesSource.js
+++ SalesNavigatorSearcherExtension/getPagesSource.js
@@ -5,7 +5,7 @@ var savedLeads = getElementsByClassName('search-nav--title Sans-16px-black-90%-b
 
 function checkType(){
 
-  console.log('savedLeads?'savedLeads);
+  console.log('savedLeads?',savedLeads);
   if (savedLeads.includes == 'Saved leads') {
     console.log('yes');
 return true;
@@ -34,35 +34,6 @@ function checkStart(){
 
 
 
-// function DOMtoString(document_root) {
-//
-//   var html = '',
-//   node = document_root.firstChild;
-//   while (node) {
-//     switch (node.nodeType) {
-//       case Node.ELEMENT_NODE:
-//       html += node.outerHTML;
-//       break;
-//       case Node.TEXT_NODE:
-//       html += node.nodeValue;
-//       break;
-//       case Node.CDATA_SECTION_NODE:
-//       html += '<![CDATA[' + node.nodeValue + ']]>';
-//       break;
-//       case Node.COMMENT_NODE:
-//       html += '<!--' + node.nodeValue + '-->';
-//       break;
-//       case Node.DOCUMENT_TYPE_NODE:
-//       // (X)HTML documents are identified by public identifiers
-//       html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-//       break;
-//     }
-//     node = node.nextSibling;
-//   }
-//   return findNames(html);
-//
-//
-// }
 
 function findNames() {
   merged = [];
